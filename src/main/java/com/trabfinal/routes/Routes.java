b48127e1288dee77dd282131c52fbe9e4089f42a
Giovanni Schenato <gischenato@gmail.com>
2022-11-10 10:36:52 -0300

    changing routes layer

--- src/main/java/com/trabfinal/routes/Routes.java
+++ src/main/java/com/trabfinal/routes/Routes.java
@@ -1,19 +1,21 @@
-package com.trabfinal;
+package com.trabfinal.routes;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class Routes {
     @Autowired
     NamedParameterJdbcTemplate jdbcTemplate;
-
+    
     @RequestMapping("/")
+    @CrossOrigin(origins = "*")
     public Map<String, String> sayHello() {
         HashMap<String, String> map = new HashMap<>();
         map.put("key", "value");
@@ -21,4 +23,5 @@ public class HelloController {
         map.put("aa", "bb");
         return map;
     }
+    
 }
